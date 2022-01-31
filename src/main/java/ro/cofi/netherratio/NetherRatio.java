ef2adc2108697256d9f20b02c35e8bd62cf334dc
CofiPRT <raresgsmart@gmail.com>
2022-01-31 09:51:23 +0200

    0.0.0-alpha: Initial commit

---
+++ src/main/java/ro/cofi/netherratio/NetherRatio.java
@@ -0,0 +1,17 @@
+package ro.cofi.netherratio;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NetherRatio extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
